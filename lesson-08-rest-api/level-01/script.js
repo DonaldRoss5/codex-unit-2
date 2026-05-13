b17b91356a089db0e100143e96680a4dd5ce20b1
dwayne ross <donaldwross24@gmail.com>
2026-05-13 14:30:07 -0500

    completed level 1 & 2

--- lesson-08-rest-api/level-01/script.js
+++ lesson-08-rest-api/level-01/script.js
@@ -1,4 +1,8 @@
 // Level 01 starter script
 // TODO: Select a target element and set its innerText to explain REST API.
-const result = document.getElementById("result");
+
+const pTag = document.getElementById("result");
+pTag.innerText =
+  "A REST API is a way for my web page to communicate with servers. to get data like map directioins, weather forecast, and product prices";
+
 // Example: result.innerText = '...'; // fill this in during the exercise
--- lesson-08-rest-api/level-02/script.js
+++ lesson-08-rest-api/level-02/script.js
@@ -1,11 +1,16 @@
 // Level 02 starter script
 // TODO: Get & save the form element to `form`, attach onsubmit, and call event.preventDefault().
+
+// TODO: set result.innerText to a short explanation about fetch
+
 const form = document.getElementById("sample-form");
+form.onsubmit = handleSubmit;
+
 const result = document.getElementById("result");
 
 function handleSubmit(event) {
   event.preventDefault();
-  // TODO: set result.innerText to a short explanation about fetch
+  const resultTag = document.getElementById("result");
+  resultTag.innerText =
+    "The fetch function takes in a URL & connects to a server";
 }
-
-if (form) form.onsubmit = handleSubmit;
