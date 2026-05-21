e67addb8cdafd99d12f204d18f14a8249110baad
dwayne ross <donaldwross24@gmail.com>
2026-05-21 15:06:39 -0500

    completed

--- lesson-07-browser-objects/level-05-querySelector/script.js
+++ lesson-07-browser-objects/level-05-querySelector/script.js
@@ -3,7 +3,6 @@
 =======
 // Level 05 — querySelector starter
 // TODO: use document.querySelector('#content') or document.querySelectorAll('.item')
-// TODO: log previous content and update nodes (iterate NodeList with forEach)
 
 // Scaffold:
 // const one = document.querySelector('#content');
@@ -13,10 +12,19 @@
 // list.forEach((el, i) => {
 //   // TODO: update each el.innerText
 // });
+
+const liTag = document.querySelector("#Two");
+
+liTag.innerHTML = "<h2>Hello World</h2>";
+
 const pTag = document.querySelector("p");
 
+<<<<<<< HEAD
 pTag.innerHTML = "<h2>I love Marcus!</h2>";
 <<<<<<< HEAD
 >>>>>>> b81312d (level 5 completed)
 =======
 >>>>>>> 8542081 (all completed)
+=======
+pTag.innerText = "I love Marcus!";
+>>>>>>> 697c6e1 (completed)
